b82c46f46a4e6f93b8264504855ad2d957864f49
mdaamir11 <91657450+mdaamir11@users.noreply.github.com>
2021-10-11 17:57:06 +0530

    Account and Wallet

---
+++ app/src/main/java/com/peceinfotech/shoppre/UI/AccountAndWallet/AccountWalletActivity.java
@@ -0,0 +1,14 @@
+package com.peceinfotech.shoppre.UI.AccountAndWallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountWalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_wallet);
+    }
+}
