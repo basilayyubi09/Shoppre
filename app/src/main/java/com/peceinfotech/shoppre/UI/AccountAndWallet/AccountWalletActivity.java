baf60e78d82c4b1048212465faf8e78e143f7f96
BasilAyyubi <basil@peaceinfotech.com>
2021-10-14 19:51:10 +0530

    before changing directory

--- app/src/main/java/com/peceinfotech/shoppre/UI/AccountAndWallet/AccountWalletActivity.java
+++ app/src/main/java/com/peceinfotech/shoppre/UI/AccountAndWallet/AccountWalletActivity.java
@@ -1,16 +1,29 @@
 package com.peceinfotech.shoppre.UI.AccountAndWallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.peceinfotech.shoppre.R;
+import com.peceinfotech.shoppre.UI.AccountAndWallet.AcountWalletFragments.ViewProfile;
+import com.peceinfotech.shoppre.UI.OnBoarding.FirstOnBoarding;
 
 public class AccountWalletActivity extends AppCompatActivity {
 
+    public static FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_wallet);
+        fragmentManager =  getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ViewProfile viewProfileFragment = new ViewProfile();
+        fragmentTransaction.add(R.id.frameLayout , viewProfileFragment , null);
+        fragmentTransaction.commit();
+
+
     }
 }
