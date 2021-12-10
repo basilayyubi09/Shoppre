3acb41818d9beeb63b4d6e4b4499fd9e0d2950ec
mdaamir11 <mdaamir@peaceinfotech.com>
2021-12-10 11:22:01 +0530

    country dropdown done

--- app/src/main/java/com/peceinfotech/shoppre/UI/Shipment/ShipmentFragment/CreateShipRequestSummaryFragment.java
+++ app/src/main/java/com/peceinfotech/shoppre/UI/Shipment/ShipmentFragment/CreateShipRequestSummaryFragment.java
@@ -1,8 +1,11 @@
 package com.peceinfotech.shoppre.UI.Shipment.ShipmentFragment;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -28,6 +32,8 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.gson.JsonObject;
 import com.hbb20.CountryCodePicker;
+import com.peceinfotech.shoppre.AccountResponse.CountryResponse;
+import com.peceinfotech.shoppre.AccountResponse.Item;
 import com.peceinfotech.shoppre.AccountResponse.RefreshTokenResponse;
 import com.peceinfotech.shoppre.AccountResponse.UpdateAddressResponse;
 import com.peceinfotech.shoppre.AuthenticationModel.DeliveryListModel;
@@ -50,7 +56,7 @@ import retrofit2.Response;
 public class CreateShipRequestSummaryFragment extends Fragment {
 
     CheckBox checkBox, checkBoxCreateShipment;
-    LinearLayout billingAddressLayout, addressForm;
+    LinearLayout billingAddressLayout, addressForm, spinnerCountryLayout;
     MaterialButton createShipBtn;
     Spinner createShipmentTitleSpinner;
     ImageView titleTriangle;
@@ -77,6 +83,8 @@ public class CreateShipRequestSummaryFragment extends Fragment {
     EditText addressLine1, addressLine2, city, state, pinCode, billingEditName;
     CardView back;
     SharedPrefManager sharedPrefManager;
+    ArrayAdapter<Item> arrayAdapter;
+    List<Item> list, duplicateList;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -128,6 +136,9 @@ public class CreateShipRequestSummaryFragment extends Fragment {
         pinCode = view.findViewById(R.id.pinCode);
         billingTitle = view.findViewById(R.id.title);
         photoCharges = view.findViewById(R.id.photoCharges);
+        spinnerCountryLayout = view.findViewById(R.id.spinnerCountryLayoutSummaryFragment);
+        setCountryList("");
+
 
         Bundle bundle = this.getArguments();
         if (bundle != null) {
@@ -159,6 +170,26 @@ public class CreateShipRequestSummaryFragment extends Fragment {
             }
 
         }
+
+
+        ////Country Dropdown
+
+        spinnerCountryLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogue();
+            }
+        });
+        spinnerCountry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinnerCountryLayout.performClick();
+            }
+        });
+
+
+
+
 
         sharedPrefManager = new SharedPrefManager(getActivity());
         setUpBillingAddress();
@@ -533,4 +564,94 @@ public class CreateShipRequestSummaryFragment extends Fragment {
         });
 
     }
+
+
+
+    private void showDialogue() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View row = getLayoutInflater().inflate(R.layout.country_dialog, null);
+
+        ListView listView = row.findViewById(R.id.dialogListView);
+        TextInputLayout inputLayout = row.findViewById(R.id.dialogSearch);
+
+
+        AlertDialog dialog = builder.create();
+        arrayAdapter = new ArrayAdapter<Item>(getActivity(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+        listView.setTextFilterEnabled(true);
+        dialog.setView(row);
+
+        listView.setTextFilterEnabled(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Object item = adapterView.getItemAtPosition(i);
+                if (item instanceof Item) {
+                    Item item1 = (Item) item;
+                    String str = ((Item) item).getCountryCode().toString().split("[\\(\\)]")[1];
+
+                }
+                spinnerCountry.setText(adapterView.getItemAtPosition(i).toString());
+                spinnerCountry.setTextColor(R.color.black);
+                inputLayout.getEditText().setText("");
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+
+        inputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.equals(null) || charSequence.length() == 0) {
+                    arrayAdapter = new ArrayAdapter<Item>(getContext(), android.R.layout.simple_list_item_1, list);
+                    listView.setAdapter(arrayAdapter);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                arrayAdapter = new ArrayAdapter<Item>(getContext(), android.R.layout.simple_list_item_1, list);
+                listView.setAdapter(arrayAdapter);
+                setCountryList(editable.toString());
+                arrayAdapter.getFilter().filter(editable);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+    }
+
+    private void setCountryList(String typedString) {
+        Call<CountryResponse> call = RetrofitClient3.getInstance3().getAppApi()
+                .getCountry("Country", typedString);
+        call.enqueue(new Callback<CountryResponse>() {
+
+            @Override
+            public void onResponse(Call<CountryResponse> call, Response<CountryResponse> response) {
+                if (response.code() == 200) {
+                    list = response.body().getItems();
+                    duplicateList = response.body().getItems();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CountryResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), t.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
